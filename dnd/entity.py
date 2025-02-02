cd9a61723c9e4e0bbc420f6ba7ffe9a127b610a0
Cynde <furlanel@usc.edu>
2025-02-03 00:39:51 +0100

    added saving throw ans skill methods directly to entity class

--- dnd/entity.py
+++ dnd/entity.py
@@ -1,11 +1,11 @@
-from typing import Dict, Optional, Any, List, Self, Literal, ClassVar, Union
+from typing import Dict, Optional, Any, List, Self, Literal, ClassVar, Union, Tuple
 from uuid import UUID, uuid4
 from pydantic import BaseModel, Field, model_validator, computed_field, field_validator
 from enum import Enum
 
 from dnd.blocks import (
     BaseBlock, AbilityScores, SavingThrowSet, Health, 
-    Equipped, Speed, ActionEconomy,SkillSet,SkillName
+    Equipped, Speed, ActionEconomy,SkillSet,SkillName,AbilityName,SkillName
 )
 from dnd.values import ModifiableValue
 from dnd.modifiers import (
@@ -52,9 +52,95 @@ class Entity(BaseBlock):
             source_entity_uuid=source_entity_uuid,
             name=name)
     
-    def get_target_entity(self) -> Optional['Entity']:
+    def get_target_entity(self,copy: bool = False) -> Optional['Entity']:
         if self.target_entity_uuid is None:
             return None
         target_entity = Entity.get(self.target_entity_uuid)
         assert isinstance(target_entity, Entity)
-        return target_entity 
+        return target_entity if not copy else target_entity.model_copy(deep=True)
+    
+    def _get_bonuses_for_skill(self, skill_name: SkillName) -> Tuple[ModifiableValue,ModifiableValue,ModifiableValue,ModifiableValue]:
+        proficiency_bonus = self.proficiency_bonus
+        skill = self.skill_set.get_skill(skill_name)
+        skill_bonus = skill.skill_bonus
+        proficiency_bonus_multiplier_callable = skill._get_proficiency_converter()
+        ability_name = skill.ability
+        ability = self.ability_scores.get_ability(ability_name)
+        ability_bonus = ability.ability_score
+        ability_modifier_bonus = ability.modifier_bonus
+        normalized_proficiency_bonus = proficiency_bonus.model_copy(deep=True)
+        normalized_proficiency_bonus.update_normalizers(proficiency_bonus_multiplier_callable)
+        return normalized_proficiency_bonus, skill_bonus, ability_bonus, ability_modifier_bonus
+    
+    def _get_bonuses_for_saving_throw(self, ability_name: AbilityName) -> Tuple[ModifiableValue,ModifiableValue,ModifiableValue,ModifiableValue]:
+        saving_throw = self.saving_throws.get_saving_throw(ability_name)
+        saving_throw_bonus = saving_throw.bonus
+        proficiency_bonus_multiplier_callable = saving_throw._get_proficiency_converter()
+        proficiency_bonus = self.proficiency_bonus
+        ability_bonus = self.ability_scores.get_ability(ability_name).ability_score
+        ability_modifier_bonus = self.ability_scores.get_ability(ability_name).modifier_bonus
+
+        normalized_proficiency_bonus = proficiency_bonus.model_copy(deep=True)
+        normalized_proficiency_bonus.update_normalizers(proficiency_bonus_multiplier_callable)
+        return normalized_proficiency_bonus, saving_throw_bonus, ability_bonus,ability_modifier_bonus
+    
+    def saving_throw_bonus(self, target_entity_uuid: Optional[UUID], ability_name: AbilityName) -> ModifiableValue:
+        if target_entity_uuid is not None:
+            self.set_target_entity(target_entity_uuid)
+        target_entity = None
+        if self.target_entity_uuid:
+            target_entity = self.get_target_entity(copy=True)
+            assert isinstance(target_entity, Entity)
+            target_entity.set_target_entity(self.uuid)
+            saving_throw_bonuses_target = target_entity._get_bonuses_for_saving_throw(ability_name)
+
+        saving_throw_bonuses_source =self._get_bonuses_for_saving_throw(ability_name)
+        if target_entity is not None:
+            for mod_source,mod_target in zip(saving_throw_bonuses_source,saving_throw_bonuses_target):
+                mod_source.set_from_target(mod_target)    
+        total_bonus_source = saving_throw_bonuses_source[0].combine_values(list(saving_throw_bonuses_source)[1:]).model_copy(deep=True)
+        self.clear_target_entity()
+
+        return total_bonus_source
+
+    def skill_bonus(self, target_entity_uuid: Optional[UUID], skill_name: SkillName) -> ModifiableValue:
+        if target_entity_uuid is not None:
+            self.set_target_entity(target_entity_uuid)
+        
+        target_entity = None
+        if self.target_entity_uuid:
+            target_entity = self.get_target_entity(copy=True)
+            assert isinstance(target_entity, Entity)
+            target_entity.set_target_entity(self.uuid)
+            skill_bonuses_target = target_entity._get_bonuses_for_skill(skill_name)
+
+        skill_bonuses_source = self._get_bonuses_for_skill(skill_name)
+        if target_entity is not None:
+            for mod_source, mod_target in zip(skill_bonuses_source, skill_bonuses_target):
+                mod_source.set_from_target(mod_target)
+        
+        total_bonus_source = skill_bonuses_source[0].combine_values(list(skill_bonuses_source)[1:]).model_copy(deep=True)
+        self.clear_target_entity()
+
+        return total_bonus_source
+
+    def skill_bonus_cross(self, target_entity_uuid: UUID, skill_name: SkillName) -> Tuple[ModifiableValue, ModifiableValue]:
+        self.set_target_entity(target_entity_uuid)
+        target_entity = self.get_target_entity(copy=True)
+        assert isinstance(target_entity, Entity)
+        target_entity.set_target_entity(self.uuid)
+
+        skill_bonuses_source = self._get_bonuses_for_skill(skill_name)
+        skill_bonuses_target = target_entity._get_bonuses_for_skill(skill_name)
+
+        for mod_source, mod_target in zip(skill_bonuses_source, skill_bonuses_target):
+            mod_target.set_from_target(mod_source)
+            mod_source.set_from_target(mod_target)
+
+        total_bonus_source = skill_bonuses_source[0].combine_values(list(skill_bonuses_source)[1:]).model_copy(deep=True)
+        total_bonus_target = skill_bonuses_target[0].combine_values(list(skill_bonuses_target)[1:]).model_copy(deep=True)
+
+        self.clear_target_entity()
+        target_entity.clear_target_entity()
+
+        return total_bonus_source, total_bonus_target
