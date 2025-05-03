e2b273c1ed6f7d3535cea5cc46cc64a9c33feb88
furlat <tfurlanello@gmail.com>
2025-05-03 15:37:17 +0200

    add backend and frontend

---
+++ ui/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+}); 
